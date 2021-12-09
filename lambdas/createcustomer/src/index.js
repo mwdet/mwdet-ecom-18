b2727702dccc09c1d8db446ea725ea3748cfa137
Hari Ravva <harikrishna.ravva@accenture.com>
2021-12-09 13:35:05 +0530

    update changes

--- lambdas/createcustomer/src/index.js
+++ lambdas/createcustomer/src/index.js
@@ -3,7 +3,7 @@ const AWS = require("aws-sdk");
 
 const docclient = new AWS.DynamoDB.DocumentClient();
 
- 
+ // test 
    exports.handler = async (event,context) => {
      console.log("Inside handler")
      console.log(event)
